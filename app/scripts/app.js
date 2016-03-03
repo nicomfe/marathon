159e5aa61cf4ef1fdbc9e4f28168794188c91d14
Nicolas Fetter <nicofetter@gmail.com>
2016-03-03 21:39:03 +1300

    Initial commit :)

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name marathonApp
+ * @description
+ * # marathonApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('marathonApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marathonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the marathonApp
+ */
+angular.module('marathonApp').controller('MainCtrl', function ($scope) {
+    jQuery(document).ready(function() {
+      /* Pop Video Box */
+      jQuery('.playVideoButton').fancybox({
+        'padding': 0,
+        'width': '100%',
+        'height': '100%',
+        'type': 'iframe'
+        });
+    });
+});
